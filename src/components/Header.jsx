470edbb002b8acba6f50077d2880d30f21cce008
princerajkr45 <priyance5204@gmail.com>
2024-08-03 22:25:15 +0530

    Input and Button component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
 function Header() {
-  const auuthStatus = useSelector((state) = state.auth.status)
+  const authStatus = useSelector((state) = state.auth.status)
   const navigate = useNavigate()
 
   const navItems = [
@@ -37,9 +37,32 @@ function Header() {
   ]
 
   return (
-    <header>
+    <header className='py-3 shadow bg-gray-500 '>
       <Container>
-
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+            </Link>
+          </div>
+          <ul>
+            {navItems.map((item) =>
+              item.active ? (
+                <li key={item.name}>
+                  <button
+                    onClick={() => navigate(item.slug)}
+                    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                  >{item.name}</button>
+                </li>
+              ) : null
+              )}
+              {authStatus && (
+                <li>
+                  <LogoutBtn />
+                </li>
+              )}
+          </ul>
+        </nav>
       </Container>
     </header>
   )
