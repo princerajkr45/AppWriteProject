3bdfaf7b9078513cc4b3239d558cc2757578b244
princerajkr45 <priyance5204@gmail.com>
2024-08-05 14:57:17 +0530

    PostForm RTE

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -2,22 +2,22 @@ import React, { useEffect, useState } from 'react'
 import { useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
 
-export default function Protected({children , authentication = true}) {
+export default function Protected({ children, authentication = true }) {
 
-    const navigate = useNavigate()
-    const[loader , setLoader] =useState()
-    const authStatus = useSelector(state =>state.auth.status)
+  const navigate = useNavigate()
+  const [loader, setLoader] = useState()
+  const authStatus = useSelector(state => state.auth.status)
 
-    useEffect(()=>{
-    if(authentication && authStatus !== authentication){
-        navigate('/login')
-    }else if(!authentication && authStatus!==authentication){
+  useEffect(() => {
+    if (authentication && authStatus !== authentication) {
+      navigate('/login')
+    } else if (!authentication && authStatus !== authentication) {
       navigate('/')
     }
 
     setLoader(false)
 
-    },[authStatus,navigate,authentication])
-  return loader? <h1>Loading....</h1>:<>{children}</>
+  }, [authStatus, navigate, authentication])
+  return loader ? <h1>Loading....</h1> : <>{children}</>
 }
 
